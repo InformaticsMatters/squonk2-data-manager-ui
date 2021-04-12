c658fdba364582f8f50cc7a2cbc59dd46e27aa2e
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2021-04-13 00:00:22 +0100

    feat(experimentation): :poop: Add basic form to test file uploads (post requests)

--- pages/data.tsx
+++ pages/data.tsx
@@ -1,25 +1,37 @@
 import { useUser } from '@auth0/nextjs-auth0';
-import {
-  useGetAvailableDatasets,
-  useGetAvailableProjects,
-  useUploadDataset,
-} from '@squonk/data-manager-client';
+import { useUploadDataset } from '@squonk/data-manager-client';
 
 import Layout from '../components/Layout';
 
 export default function Index() {
   const { user, error, isLoading } = useUser();
 
-  const { data: datasets } = useGetAvailableDatasets();
-  const { data: projects } = useGetAvailableProjects();
+  // const { data: datasets } = useGetAvailableDatasets();
+  // const { data: projects } = useGetAvailableProjects();
   const uploadDatasetMutation = useUploadDataset();
 
   return (
     <Layout user={user} authLoading={isLoading} authError={error}>
       <h1>Data Page</h1>
-      {JSON.stringify(datasets)}
-      {JSON.stringify(projects)}
-      <button onClick={() => uploadDatasetMutation.mutate({ data: 1 })}>Upload</button>
+      {/* {JSON.stringify(datasets)}
+      {JSON.stringify(projects)} */}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const files = (e.target as any)['files'].files;
+
+          uploadDatasetMutation.mutate({
+            data: {
+              as_filename: 'test_name.pdb',
+              dataset_file: files[0],
+              dataset_type: 'chemical/x-pdb',
+            },
+          });
+        }}
+      >
+        <input type="file" name="files" multiple />
+        <input type="submit" name="submit" />
+      </form>
     </Layout>
   );
 }

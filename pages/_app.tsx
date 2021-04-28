ed6496c40a97f405e04bd6ee260179d4fc166393
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2021-04-28 22:29:29 +0100

    fix(react-query): :ambulance: Implement properly the query provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,13 +12,11 @@ import { CssBaseline, useTheme } from '@material-ui/core';
 import { setBaseUrl } from '@squonk/data-manager-client';
 import { MuiTheme } from '@squonk/mui-theme';
 
-export const queryClient = new QueryClient();
-
 // ? Is this the right place to set this?
 setBaseUrl('/api/dm-api');
 
 export default function App({ Component, pageProps }: AppProps) {
-  // ! Need resolutions in package.json for <Theme></Theme> to not cause "invalid hooks usage" error
+  // ! Need resolutions in monorepo package.json for <Theme></Theme> to not cause "invalid hooks usage" error
 
   const queryClientRef = useRef<QueryClient | null>(null);
 
@@ -32,7 +30,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <CssBaseline />
       <ThemeProvider theme={theme}>
         <UserProvider>
-          <QueryClientProvider client={queryClient}>
+          <QueryClientProvider client={queryClientRef.current}>
             <Hydrate state={pageProps.dehydratedState}>
               <Component {...pageProps} />
             </Hydrate>

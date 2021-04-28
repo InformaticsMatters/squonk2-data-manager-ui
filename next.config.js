1656b85c174331e85bb83fd5ab68d7dbe2abfe4e
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2021-04-28 11:54:11 +0100

    fix(fonts): :bug: Add global sass varaible (via next config) to @font-face urls to use env variable so that we can access fonts from staging site

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   basePath: process.env.BASE_PATH,
+  sassOptions: {
+    prependData: `$assetsURL: '${
+      process.env.NODE_ENV === 'development' ? 'https://squonk.informaticsmatters.org' : '.'
+    }';`,
+  },
   future: {
     webpack5: true,
   },

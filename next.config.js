70062970dffdffbd990aaef7c24d0ee9a9231f52
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2021-05-10 18:02:08 +0100

    fix(env-vars): Fix nextjs build failing due to Sass env var
    It now sources the correct env var when in prod

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@ module.exports = {
   basePath: process.env.BASE_PATH,
   sassOptions: {
     prependData: `$assetsURL: '${
-      process.env.NODE_ENV === 'development' ? 'https://squonk.informaticsmatters.org' : '.'
+      process.env.NODE_ENV === 'development'
+        ? 'https://squonk.informaticsmatters.org'
+        : process.env.BASE_URL
     }';`,
   },
   future: {

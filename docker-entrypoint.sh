cf1845246a1435f6f7e81b60b8256d68061506e9
Alan Christie <alan.christie@matildapeak.com>
2021-06-01 12:00:35 +0100

    - Nobbles entrypoint (i.e. does nothing at runtime)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,10 +4,10 @@
 # We do this to handle URL and Paths set in the .env
 # (which is 'injected' as a config map when run in Kubernetes).
 # A 'default' is copied into the image using the build 'FLAVOUR'.
-echo "+> Building"
-yarn build
-echo "+> Installing"
-yarn install --production --ignore-scripts --prefer-offline
+#echo "+> Building"
+#yarn build
 
-echo "+> Running"
-yarn start
+#echo "+> Running"
+#yarn start
+
+sleep inifinity

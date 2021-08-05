f43838ecde33db3dfe7ab27f4403a72476529702
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2021-08-05 11:44:26 +0100

    feat(job-file-inputs): Add collapse animation when toggling visibility of file selector

--- components/ExecutionsCard/FileSelector.tsx
+++ components/ExecutionsCard/FileSelector.tsx
@@ -2,7 +2,7 @@ import type { FC } from 'react';
 import React, { useState } from 'react';
 
 import { css } from '@emotion/react';
-import { Button, Toolbar, Typography } from '@material-ui/core';
+import { Button, Collapse, Toolbar, Typography } from '@material-ui/core';
 
 import type { ProjectId } from '../state/currentProjectHooks';
 import { MiniFileList } from './MiniFileList';
@@ -17,12 +17,33 @@ export interface FileSelectorProps {
 
 export const FileSelector: FC<FileSelectorProps> = ({ value, type, ...props }) => {
   const [expanded, setExpanded] = useState(false);
+  const numberOfSelectedFiles = [value].flat().filter((f) => f !== undefined).length;
 
-  if (expanded) {
-    return (
-      <>
-        <MiniFileList {...props} type={type} value={value} />
-        <Toolbar disableGutters variant="dense">
+  return (
+    <>
+      <Collapse in={expanded}>
+        <div>
+          <MiniFileList {...props} type={type} value={value} />
+        </div>
+      </Collapse>
+      <Toolbar disableGutters variant="dense">
+        {!expanded ? (
+          <>
+            <Typography display="inline" variant="body2">
+              Selected Files: {numberOfSelectedFiles > 0 ? <b>{numberOfSelectedFiles}</b> : 'None'}
+            </Typography>
+            <Button
+              css={css`
+                margin-left: auto;
+              `}
+              size="small"
+              variant="outlined"
+              onClick={() => setExpanded(true)}
+            >
+              Select {type}
+            </Button>
+          </>
+        ) : (
           <Button
             css={css`
               margin-left: auto;
@@ -33,28 +54,8 @@ export const FileSelector: FC<FileSelectorProps> = ({ value, type, ...props }) =
           >
             Close
           </Button>
-        </Toolbar>
-      </>
-    );
-  }
-
-  const numberOfSelectedFiles = [value].flat().filter((f) => f !== undefined).length;
-
-  return (
-    <Toolbar disableGutters variant="dense">
-      <Typography display="inline" variant="body2">
-        Selected Files: {numberOfSelectedFiles > 0 ? <b>{numberOfSelectedFiles}</b> : 'None'}
-      </Typography>
-      <Button
-        css={css`
-          margin-left: auto;
-        `}
-        size="small"
-        variant="outlined"
-        onClick={() => setExpanded(true)}
-      >
-        Select {type}
-      </Button>
-    </Toolbar>
+        )}
+      </Toolbar>
+    </>
   );
 };

f6f12dda2654d48ee41fbb9ecf4e007d011e95b8
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2021-08-27 15:27:39 +0100

    fix(labels): Add missing tooltip to add label icon button

--- components/DatasetDetails/NewLabelButton.tsx
+++ components/DatasetDetails/NewLabelButton.tsx
@@ -6,7 +6,7 @@ import type { DatasetVersionDetail } from '@squonk/data-manager-client';
 import { getGetDatasetsQueryKey, useAddAnnotations } from '@squonk/data-manager-client/dataset';
 
 import { css } from '@emotion/react';
-import { Button, IconButton, Popover } from '@material-ui/core';
+import { Button, IconButton, Popover, Tooltip } from '@material-ui/core';
 import { useTheme } from '@material-ui/core';
 import AddCircleOutlineRoundedIcon from '@material-ui/icons/AddCircleOutlineRounded';
 import { Field, Form, Formik } from 'formik';
@@ -36,9 +36,11 @@ export const NewLabelButton: FC<NewLabelButtonProps> = ({ datasetId, datasetVers
 
   return (
     <>
-      <IconButton size="small" {...bindTrigger(popupState)}>
-        <AddCircleOutlineRoundedIcon />
-      </IconButton>
+      <Tooltip title="Add a new label">
+        <IconButton size="small" {...bindTrigger(popupState)}>
+          <AddCircleOutlineRoundedIcon />
+        </IconButton>
+      </Tooltip>
       <Popover
         {...bindPopover(popupState)}
         anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}

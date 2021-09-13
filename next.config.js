9a9cd260112ae3ec0930e0d30e9126460760edf9
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2021-09-13 12:04:26 +0100

    build(mdx): Switch to xdm and change babel config to use new jdx runtimes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
-const withMDX = require('@next/mdx')({
-  extension: /\.(md|mdx)$/,
-});
-
 let ASSET_URL;
 
 if (process.env.NODE_ENV === 'production' && !process.env.BASE_URL.includes('localhost')) {
@@ -9,12 +5,31 @@ if (process.env.NODE_ENV === 'production' && !process.env.BASE_URL.includes('loc
 }
 ASSET_URL = process.env.BASE_URL = 'https://squonk.informaticsmatters.org';
 
-module.exports = withMDX({
+/** @type {import('next').NextConfig} */
+module.exports = {
   // reactStrictMode: true, // TODO: switch on after MUI-v5 switch
-  pageExtensions: ['js', 'ts', 'jsx', 'tsx', 'md', 'mdx'],
+  pageExtensions: ['js', 'ts', 'jsx', 'tsx', 'mdx'],
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   sassOptions: {
     prependData: `$assetsURL: '${ASSET_URL}';`,
   },
-  // experimental: { esmExternals: true },
-});
+  // Allow mdx content and mdx files as pages
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.mdx$/,
+      use: [
+        {
+          loader: 'xdm/webpack.cjs',
+          options: {
+            // This line allows a configurable provider to configure transformed components
+            providerImportSource: '@mdx-js/react',
+            // This line provides compatibility with the emotion css prop jsx pragma
+            jsxImportSource: '@emotion/react',
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};

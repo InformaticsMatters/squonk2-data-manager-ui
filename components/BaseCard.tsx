12ba7ead73473bd2382621191aef7cf9078867e2
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2021-09-21 23:06:05 +0100

    docs(base-card): Improve comments of BaseCard component and props interface

--- components/BaseCard.tsx
+++ components/BaseCard.tsx
@@ -14,14 +14,37 @@ import {
 } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
+/**
+ * Parameters passed to actions when a component is passed
+ * {@label ActionsParams}
+ */
 interface ActionsParams {
   setExpanded: (isExpanded: boolean) => void;
 }
 
 interface BaseCardProps {
+  /**
+   * ReactNode or Component to render in the <CardActions> component. Optional - nothing is
+   * displayed if not passed.
+   *
+   * If a component is passed, {@link ActionsParams} is provided as render props.
+   *
+   * {@label actions}
+   */
   actions?: React.ReactNode | ((actionsParams: ActionsParams) => React.ReactNode);
+  /**
+   * ReactNode to be rendered inside the collapsed part of the card. Optional - nothing is
+   * displayed if not passed.
+   */
   collapsed?: React.ReactNode;
+  /**
+   * Whether the card is collapsed by default. Optional. Defaults to `true`.
+   */
   collapsedByDefault?: boolean;
+  /**
+   * String values to be displayed in the header section of the card. Only title is required.
+   * Missing values are not displayed.
+   */
   header?: {
     title: string;
     subtitle?: string;
@@ -30,6 +53,15 @@ interface BaseCardProps {
   };
 }
 
+/**
+ * Generic card component with actions and an expanded view.
+ *
+ * Includes:
+ * * a header including an icon (MuiAvatar)
+ * * optional actions that are always displayed - {@link actions}
+ * * an optional collapsed view that can be enabled by default
+ * * children are passed into the main area (unexpanded view) of the component
+ */
 export const BaseCard: FC<BaseCardProps> = ({
   children,
   actions,
@@ -72,6 +104,8 @@ export const BaseCard: FC<BaseCardProps> = ({
           justify-content: right;
         `}
       >
+        {/* ? should this be a functionCall() or a <ReactElement />
+        or should this be separate props with a union and one a never type */}
         {typeof actions === 'function' ? actions({ setExpanded }) : actions}
         {collapsed !== undefined && (
           <IconButton

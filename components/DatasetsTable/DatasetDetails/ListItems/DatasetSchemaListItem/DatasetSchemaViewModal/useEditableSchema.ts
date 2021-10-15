27ffa827edb9145c80d80048d85bad7623a52070
Anton Gasperak <anton.gasperak@m2ms.sk>
2021-10-15 14:59:09 +0200

    refactor(dataset-schema): Use immer for useEditableSchema reducer

--- components/DatasetsTable/DatasetDetails/ListItems/DatasetSchemaListItem/DatasetSchemaViewModal/useEditableSchema.ts
+++ components/DatasetsTable/DatasetDetails/ListItems/DatasetSchemaListItem/DatasetSchemaViewModal/useEditableSchema.ts
@@ -1,4 +1,6 @@
-import { useCallback, useEffect, useMemo, useReducer } from 'react';
+import { useCallback, useEffect, useMemo } from 'react';
+
+import { useImmerReducer } from 'use-immer';
 
 import type { Field, FieldKey, Fields, FieldValue, TypedSchema } from './types';
 
@@ -19,9 +21,9 @@ type EditableSchemaState = {
 } | null;
 
 const editableSchemaReducer = <K extends FieldKey, V extends FieldValue<K>>(
-  state: EditableSchemaState,
+  draft: EditableSchemaState,
   action: EditableSchemaStateAction<K, V>,
-): EditableSchemaState => {
+): EditableSchemaState | void => {
   switch (action.type) {
     case 'clear': {
       return null;
@@ -35,57 +37,45 @@ const editableSchemaReducer = <K extends FieldKey, V extends FieldValue<K>>(
       };
     }
     case 'changeField': {
-      if (state) {
+      if (draft) {
         const { fieldName, fieldKey, fieldValue } = action;
-        const editedSchemaProps = {
-          ...state.editedSchemaProps,
-          fields: { ...state.editedSchemaProps.fields },
-        };
 
-        const editedField = editedSchemaProps.fields[fieldName] || {};
+        // Has to be extracted like this because TS would complain of possibly undefined value.
+        const editedField = draft.editedSchemaProps.fields[fieldName] || {};
 
-        if (state.originalSchema.fields[fieldName][fieldKey] === fieldValue) {
+        // If the provided field value equals to the original, unset the field key in the helper
+        // object.
+        if (draft.originalSchema.fields[fieldName][fieldKey] === fieldValue) {
           delete editedField[fieldKey];
 
+          // If there are no keys remaining for the field, delete the whole field from the helper
+          // object.
           if (!Object.keys(editedField).length) {
-            delete editedSchemaProps.fields[fieldName];
+            delete draft.editedSchemaProps.fields[fieldName];
           } else {
-            editedSchemaProps.fields[fieldName] = editedField;
+            draft.editedSchemaProps.fields[fieldName] = editedField;
           }
         } else {
           editedField[fieldKey] = fieldValue;
 
-          editedSchemaProps.fields[fieldName] = editedField;
+          draft.editedSchemaProps.fields[fieldName] = editedField;
         }
-
-        return {
-          ...state,
-          editedSchemaProps,
-        };
       }
-      return state;
+      break;
     }
     case 'changeDescription': {
-      if (state) {
-        const editedSchemaProps = {
-          ...state.editedSchemaProps,
-        };
-
-        if (state.originalSchema.description === action.description) {
-          delete editedSchemaProps.description;
+      if (draft) {
+        // Same explanation like the case above.
+        if (draft.originalSchema.description === action.description) {
+          delete draft.editedSchemaProps.description;
         } else {
-          editedSchemaProps.description = action.description;
+          draft.editedSchemaProps.description = action.description;
         }
-
-        return {
-          ...state,
-          editedSchemaProps,
-        };
       }
-      return state;
+      break;
     }
     default:
-      return state;
+      break;
   }
 };
 
@@ -95,7 +85,10 @@ const editableSchemaReducer = <K extends FieldKey, V extends FieldValue<K>>(
  * well as the edited schema, which is composed from the helper object and the original schema.
  */
 export const useEditableSchemaView = (originalSchema?: TypedSchema) => {
-  const [editableSchemaState, dispatch] = useReducer(editableSchemaReducer, null);
+  const [editableSchemaState, dispatch] = useImmerReducer<EditableSchemaState>(
+    editableSchemaReducer,
+    null,
+  );
 
   useEffect(() => {
     // If no original schema has been provided, return undefined state.
@@ -104,7 +97,7 @@ export const useEditableSchemaView = (originalSchema?: TypedSchema) => {
     } else {
       dispatch({ type: 'clear' });
     }
-  }, [originalSchema]);
+  }, [originalSchema, dispatch]);
 
   // Calculates the current state of the schema. It loops through the original schema and looks for
   // changes to its fields in the helper object. If it finds changes, it uses those. If it doesn't,
@@ -155,12 +148,15 @@ export const useEditableSchemaView = (originalSchema?: TypedSchema) => {
     ) => {
       dispatch({ type: 'changeField', fieldName, fieldKey, fieldValue });
     },
-    [],
+    [dispatch],
   );
 
-  const setSchemaDescription = useCallback((description: string) => {
-    dispatch({ type: 'changeDescription', description });
-  }, []);
+  const setSchemaDescription = useCallback(
+    (description: string) => {
+      dispatch({ type: 'changeDescription', description });
+    },
+    [dispatch],
+  );
 
   // Returns the changes in the form of the original schema. `Partial` is used because not every
   // field of the schema has to be changed.

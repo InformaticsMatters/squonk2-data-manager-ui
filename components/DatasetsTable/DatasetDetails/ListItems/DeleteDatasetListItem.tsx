7fb51e889d6d6b547a33876e52c5e677fdbef4e7
Anton Gasperak <anton.gasperak@m2ms.sk>
2021-11-10 14:29:39 +0100

    refactor(dataset-details): Adjust list item buttons behavior

--- components/DatasetsTable/DatasetDetails/ListItems/DeleteDatasetListItem.tsx
+++ components/DatasetsTable/DatasetDetails/ListItems/DeleteDatasetListItem.tsx
@@ -4,8 +4,7 @@ import { useQueryClient } from 'react-query';
 import type { DatasetVersionSummary } from '@squonk/data-manager-client';
 import { getGetDatasetsQueryKey, useDeleteDataset } from '@squonk/data-manager-client/dataset';
 
-import { ListItem, ListItemSecondaryAction, ListItemText } from '@material-ui/core';
-import { IconButton } from '@material-ui/core';
+import { ListItem, ListItemText } from '@material-ui/core';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 
 import { WarningDeleteButton } from '../../../WarningDeleteButton';
@@ -47,16 +46,7 @@ export const DeleteDatasetListItem = ({ datasetId, version, onDelete }: DeleteDa
       {({ isDeleting, openModal }) => (
         <ListItem button disabled={isDeleting} onClick={openModal}>
           <ListItemText primary="Delete this Version of the Dataset" />
-          <ListItemSecondaryAction>
-            <IconButton
-              aria-label="Delete selected dataset"
-              disabled={isDeleting}
-              edge="end"
-              onClick={openModal}
-            >
-              <DeleteForeverIcon />
-            </IconButton>
-          </ListItemSecondaryAction>
+          <DeleteForeverIcon color="action" />
         </ListItem>
       )}
     </WarningDeleteButton>

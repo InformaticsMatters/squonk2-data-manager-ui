88c5bc8321469304baea78bc6672da0750d88ca3
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2021-12-07 16:33:46 +0000

    fix(proxy): Provide correct error message when AS env var isn't provided

--- pages/api/as-api/[...as-proxy].ts
+++ pages/api/as-api/[...as-proxy].ts
@@ -4,7 +4,7 @@ export { config } from '../../../utils/api/apiProxy';
 const target = process.env.ACCOUNT_SERVER_API_SERVER;
 
 if (target === undefined) {
-  throw Error('Data Manager API environment variable not specified!');
+  throw Error('Account Server API environment variable not specified!');
 }
 
 export default createProxyMiddleware(`^/api/as-api`, target);

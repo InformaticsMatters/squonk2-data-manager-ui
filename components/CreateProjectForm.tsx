00aa06223dd8816d9fefc5ec42f3ccc2646b780a
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2022-04-25 19:49:41 +0100

    refactor(create-project): Refactor both create project components into one

---
+++ components/CreateProjectForm.tsx
@@ -0,0 +1,191 @@
+import { useQueryClient } from 'react-query';
+
+import type { AsError, UnitProductPostBodyBodyFlavour } from '@squonk/account-server-client';
+import {
+  getGetProductsForUnitQueryKey,
+  useCreateUnitProduct,
+  useGetProductTypes,
+} from '@squonk/account-server-client/product';
+import { getGetUnitsQueryKey } from '@squonk/account-server-client/unit';
+import type { DmError } from '@squonk/data-manager-client';
+import { getGetProjectsQueryKey, useCreateProject } from '@squonk/data-manager-client/project';
+import { getGetUserAccountQueryKey } from '@squonk/data-manager-client/user';
+
+import { css } from '@emotion/react';
+import { Button, MenuItem, Typography, useMediaQuery, useTheme } from '@material-ui/core';
+import type { FormikConfig } from 'formik';
+import { Field, Form, Formik } from 'formik';
+import { TextField } from 'formik-material-ui';
+import * as yup from 'yup';
+
+import { PROJECT_SUB } from '../constants/products';
+import { useCurrentProjectId } from '../hooks/projectHooks';
+import { useEnqueueError } from '../hooks/useEnqueueStackError';
+import type { Resolve } from '../types';
+import { getErrorMessage } from '../utils/orvalError';
+import { formatTierString, getBillingDay } from '../utils/productUtils';
+import type { FormikModalWrapperProps } from './modals/FormikModalWrapper';
+import { FormikModalWrapper } from './modals/FormikModalWrapper';
+
+export type OrgAndUnitIdTuple = [organisationId: string, unitId: string];
+
+export interface CreateProjectFormProps {
+  modal?: Resolve<
+    Pick<FormikModalWrapperProps, 'id' | 'title' | 'submitText' | 'open' | 'onClose'>
+  >;
+  orgAndUnit: Resolve<OrgAndUnitIdTuple> | (() => Promise<OrgAndUnitIdTuple>);
+}
+
+const getIds = async (orgAndUnit: CreateProjectFormProps['orgAndUnit']) => {
+  if (typeof orgAndUnit === 'function') {
+    return orgAndUnit();
+  }
+  return orgAndUnit;
+};
+
+const initialValues = { projectName: '', flavour: '' };
+
+type ProjectFormikProps = FormikConfig<typeof initialValues>;
+
+export const CreateProjectForm = ({ modal, orgAndUnit }: CreateProjectFormProps) => {
+  const theme = useTheme();
+  const biggerThanSm = useMediaQuery(theme.breakpoints.up('sm'));
+
+  const queryClient = useQueryClient();
+
+  const { data, isLoading, isError, error } = useGetProductTypes();
+  const productTypes = data?.product_types.filter(
+    (productType) => productType.type === PROJECT_SUB,
+  );
+
+  const { mutateAsync: createProject } = useCreateProject();
+  const { mutateAsync: createProduct } = useCreateUnitProduct();
+
+  const { enqueueError, enqueueSnackbar } = useEnqueueError<DmError | AsError>();
+
+  const { setCurrentProjectId } = useCurrentProjectId();
+
+  const create = async (projectName: string, flavour: string) => {
+    const [organisationId, unitId] = await getIds(orgAndUnit);
+    // Get the (possibly optional) service ID from the selected product type
+    const serviceId = productTypes?.find(
+      (productType) => productType.flavour === flavour && productType.type === PROJECT_SUB,
+    )?.service;
+    // The service ID is required so we error here - sadly there isn't much the user can do here
+    // This would only happen due to an API error
+    // TODO: Report this to Sentry
+    if (serviceId === undefined) {
+      throw new Error('Service ID was missing in the product type. ');
+    }
+
+    const { id: productId } = await createProduct({
+      unitId,
+      data: {
+        name: projectName,
+        type: PROJECT_SUB,
+        billing_day: getBillingDay(),
+        flavour: flavour as UnitProductPostBodyBodyFlavour,
+        service_id: serviceId.id,
+      },
+    });
+
+    const { project_id } = await createProject({
+      data: {
+        name: projectName,
+        organisation_id: organisationId,
+        unit_id: unitId,
+        tier_product_id: productId,
+      },
+    });
+
+    enqueueSnackbar('Project created');
+
+    queryClient.invalidateQueries(getGetProjectsQueryKey());
+    queryClient.invalidateQueries(getGetUserAccountQueryKey());
+
+    queryClient.invalidateQueries(getGetProductsForUnitQueryKey(unitId));
+    queryClient.invalidateQueries(getGetUnitsQueryKey());
+
+    setCurrentProjectId(project_id);
+  };
+
+  const handleSubmit: ProjectFormikProps['onSubmit'] = async (
+    { projectName, flavour },
+    { setSubmitting },
+  ) => {
+    try {
+      await create(projectName, flavour);
+    } catch (error) {
+      enqueueError(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  const children: ProjectFormikProps['children'] = ({ submitForm, isSubmitting, isValid }) => (
+    <Form
+      css={css`
+        margin-top: ${theme.spacing()}px;
+      `}
+    >
+      <div
+        // Make this a styled component
+        css={css`
+          display: grid;
+          grid-template-columns: ${biggerThanSm ? '1fr 1fr auto' : '1fr'};
+          gap: ${theme.spacing()}px;
+          align-items: flex-start;
+          .MuiInputBase-input {
+            background: ${theme.palette.background.paper};
+            border-radius: 4px;
+          }
+        `}
+      >
+        <Field autoFocus fullWidth component={TextField} label="Project Name" name="projectName" />
+        {isError ? (
+          <Typography color="error">{getErrorMessage(error)}</Typography>
+        ) : (
+          <Field fullWidth select component={TextField} label="Tier" name="flavour">
+            {isLoading ? (
+              <MenuItem disabled>Loading</MenuItem>
+            ) : (
+              productTypes?.map((product) => {
+                return (
+                  <MenuItem key={product.flavour} value={product.flavour}>
+                    {formatTierString(product.flavour ?? 'Unknown Flavour')}
+                  </MenuItem>
+                );
+              })
+            )}
+          </Field>
+        )}
+        {!modal && (
+          <Button disabled={isSubmitting || !isValid} onClick={submitForm}>
+            Create
+          </Button>
+        )}
+      </div>
+    </Form>
+  );
+
+  const commonProps: ProjectFormikProps = {
+    validateOnMount: true,
+    initialValues,
+    validationSchema: yup.object().shape({
+      projectName: yup
+        .string()
+        .required('A project name is required')
+        .matches(/^[A-Za-z0-9-_.][A-Za-z0-9-_. ]*[A-Za-z0-9-_.]$/),
+      flavour: yup.string().required('A tier is required'),
+    }),
+    onSubmit: handleSubmit,
+  };
+
+  return modal ? (
+    <FormikModalWrapper {...modal} {...commonProps}>
+      {children}
+    </FormikModalWrapper>
+  ) : (
+    <Formik {...commonProps}>{children}</Formik>
+  );
+};
--- components/UserBootstrapper/BootstrapAlert/BootstrapForm.tsx
+++ components/UserBootstrapper/BootstrapAlert/BootstrapForm.tsx
@@ -1,160 +1,20 @@
-import { useQueryClient } from 'react-query';
+import { useCreateDefaultUnit } from '@squonk/account-server-client/unit';
 
-import type {
-  AsError,
-  UnitProductPostBodyBody,
-  UnitProductPostBodyBodyFlavour,
-} from '@squonk/account-server-client';
-import { useCreateUnitProduct } from '@squonk/account-server-client/product';
-import { getGetUnitsQueryKey, useCreateDefaultUnit } from '@squonk/account-server-client/unit';
-import type { DmError } from '@squonk/data-manager-client';
-import { getGetProjectsQueryKey, useCreateProject } from '@squonk/data-manager-client/project';
-import { getGetUserAccountQueryKey } from '@squonk/data-manager-client/user';
-
-import { css } from '@emotion/react';
-import { Button, MenuItem, Typography, useMediaQuery, useTheme } from '@material-ui/core';
-import { Field, Form, Formik } from 'formik';
-import { TextField } from 'formik-material-ui';
-import * as yup from 'yup';
-
-import { useCurrentProjectId } from '../../../hooks/projectHooks';
-import { useEnqueueError } from '../../../hooks/useEnqueueStackError';
-import { useGetProjectProductTypes } from '../../../hooks/useGetProjectProductTypes';
-import { getErrorMessage } from '../../../utils/orvalError';
-import { formatTierString, getBillingDay } from '../../../utils/productUtils';
+import type { OrgAndUnitIdTuple } from '../../CreateProjectForm';
+import { CreateProjectForm } from '../../CreateProjectForm';
 
 /**
  * Form for creating a default unit with a project.
  */
 export const BootstrapForm = () => {
-  const theme = useTheme();
-  const biggerThanSm = useMediaQuery(theme.breakpoints.up('sm'));
-
-  const queryClient = useQueryClient();
-
-  const {
-    projectProductTypes,
-    isLoading: isLoadingProductTypes,
-    isError: isProductsTypesError,
-    error: productTypesError,
-  } = useGetProjectProductTypes();
-
   const { mutateAsync: createUnit } = useCreateDefaultUnit();
-  const { mutateAsync: createProject } = useCreateProject();
-  const { mutateAsync: createProduct } = useCreateUnitProduct();
-
-  const { enqueueError, enqueueSnackbar } = useEnqueueError<DmError | AsError>();
-
-  const { setCurrentProjectId } = useCurrentProjectId();
-
-  const create = async (projectName: string, flavour: string, serviceId: number) => {
-    const { id: unitId, organisation_id } = await createUnit();
-
-    const { id: productId } = await createProduct({
-      unitId,
-      data: {
-        name: projectName,
-        type: 'DATA_MANAGER_PROJECT_TIER_SUBSCRIPTION',
-        billing_day: getBillingDay(),
-        flavour: flavour as UnitProductPostBodyBodyFlavour, // TODO is this recent?
-        service_id: serviceId, // TODO this is missing as well
-      } as UnitProductPostBodyBody,
-    });
-
-    const { project_id } = await createProject({
-      data: {
-        name: projectName,
-        organisation_id,
-        unit_id: unitId,
-        tier_product_id: productId,
-      },
-    });
-
-    enqueueSnackbar('Project created', { variant: 'success' });
-
-    // DM
-    queryClient.invalidateQueries(getGetProjectsQueryKey());
-    queryClient.invalidateQueries(getGetUserAccountQueryKey());
-
-    // AS
-    queryClient.invalidateQueries(getGetUnitsQueryKey());
-
-    setCurrentProjectId(project_id);
-  };
 
   return (
-    <Formik
-      validateOnMount
-      initialValues={{ projectName: '', flavour: '', serviceId: 0 }}
-      validationSchema={yup.object().shape({
-        projectName: yup
-          .string()
-          .required('A project name is required')
-          .min(2, 'The name is too short'),
-        flavour: yup.string().required('A tier must be selected'),
-      })}
-      onSubmit={async ({ projectName, flavour, serviceId }, { setSubmitting }) => {
-        try {
-          await create(projectName, flavour, serviceId);
-        } catch (error) {
-          enqueueError(error);
-        } finally {
-          setSubmitting(false);
-        }
+    <CreateProjectForm
+      orgAndUnit={async () => {
+        const { id: unitId, organisation_id } = await createUnit();
+        return [organisation_id, unitId] as OrgAndUnitIdTuple;
       }}
-    >
-      {({ submitForm, isSubmitting, isValid, setFieldValue }) => (
-        <Form
-          css={css`
-            margin-top: ${theme.spacing()}px;
-          `}
-        >
-          <div
-            css={css`
-              display: grid;
-              grid-template-columns: ${biggerThanSm ? '1fr 1fr auto' : '1fr'};
-              gap: ${theme.spacing()}px;
-              align-items: flex-start;
-              .MuiInputBase-input {
-                background: ${theme.palette.background.paper};
-                border-radius: 4px;
-              }
-            `}
-          >
-            <Field
-              autoFocus
-              fullWidth
-              component={TextField}
-              label="Project Name"
-              name="projectName"
-            />
-            {isProductsTypesError ? (
-              <Typography color="error">{getErrorMessage(productTypesError)}</Typography>
-            ) : (
-              <Field fullWidth select component={TextField} label="Tier" name="flavour">
-                {isLoadingProductTypes ? (
-                  <MenuItem disabled>Loading</MenuItem>
-                ) : (
-                  projectProductTypes?.map((product) => {
-                    return (
-                      <MenuItem
-                        key={product.flavour}
-                        value={product.flavour}
-                        onClick={() => setFieldValue('serviceId', product.service.id)}
-                      >
-                        {formatTierString(product.flavour)}
-                      </MenuItem>
-                    );
-                  })
-                )}
-              </Field>
-            )}
-            <Button disabled={isSubmitting || !isValid} onClick={submitForm}>
-              Create
-            </Button>
-          </div>
-        </Form>
-      )}
-    </Formik>
+    />
   );
 };

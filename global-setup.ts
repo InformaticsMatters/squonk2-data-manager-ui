13d1ae80270c43007de8b093006aecb67bdda161
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2022-05-09 23:04:11 +0100

    test: Run tests on prod build instead of dev

--- global-setup.ts
+++ global-setup.ts
@@ -1,7 +1,7 @@
 import type { FullConfig } from '@playwright/test';
 import { chromium } from '@playwright/test';
 
-const { BASE_URL, BASE_PATH, PW_USERNAME, PW_PASSWORD } = process.env;
+const { BASE_URL, BASE_PATH = '', PW_USERNAME, PW_PASSWORD } = process.env;
 
 const globalSetup = async (_config: FullConfig) => {
   const baseURL = '' + BASE_URL + BASE_PATH;
@@ -10,14 +10,14 @@ const globalSetup = async (_config: FullConfig) => {
   const page = await browser.newPage();
   await page.goto(baseURL, { timeout: 60000 });
   await page.click('button');
-  await page.click('text=Login', { timeout: 60000 });
+  await page.click('text=Login');
 
   await page.type('input[name=username]', PW_USERNAME as string);
   await page.type('input[name=password]', PW_PASSWORD as string);
 
   await page.click('input:has-text("Log In")');
 
-  await page.waitForURL(baseURL);
+  await page.waitForURL(baseURL, { timeout: 60000 });
   // Save signed-in state to 'storageState.json'.
   await page.context().storageState({ path: 'storageState.json' });
   await browser.close();

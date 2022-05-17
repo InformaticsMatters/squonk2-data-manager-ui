59574e306808d9bda0acc19019a93bb186e62517
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2022-05-17 23:15:27 +0100

    feat(instance): Expose debug when running Jobs/Apps
    Fixes #475

---
+++ components/executionsCards/DebugCheckbox.tsx
@@ -0,0 +1,27 @@
+import { Box, Checkbox, FormControlLabel, Typography } from '@mui/material';
+
+export type DebugValue = '0' | 'debug';
+
+export interface DebugCheckboxProps {
+  value: DebugValue;
+  onChange: (value: DebugValue) => void;
+}
+
+export const DebugCheckbox = ({ value, onChange }: DebugCheckboxProps) => {
+  return (
+    <Box marginX={1}>
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={value === '0' ? false : true}
+            onChange={(_event, checked) => onChange(checked ? 'debug' : '0')}
+          />
+        }
+        label="Debug"
+      />
+      <Typography gutterBottom variant="body2">
+        Enabling this allows an admin to inspect the instance fully once it finishes.
+      </Typography>
+    </Box>
+  );
+};

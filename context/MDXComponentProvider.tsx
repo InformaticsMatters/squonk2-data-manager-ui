e9049eeb89958498f4e34aa45418a63c3909b50e
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2022-05-26 10:45:27 +0100

    docs: Update links in docs pages

--- context/MDXComponentProvider.tsx
+++ context/MDXComponentProvider.tsx
@@ -1,16 +1,26 @@
-import type { FC } from 'react';
+import type { AnchorHTMLAttributes, DetailedHTMLProps, FC } from 'react';
 
 import { MDXProvider } from '@mdx-js/react';
 import { Link, Typography } from '@mui/material';
+import NextLink from 'next/link';
+
+type LinkProps = DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>;
 
 export const MDXComponentProvider: FC = ({ children }) => {
   return (
     <MDXProvider
       components={{
         p: (props: unknown) => <Typography component="p" {...props} gutterBottom />,
-        a: (props: unknown) => <Link {...props} variant="body1" />,
+        a: (props: unknown) => (
+          <Link rel="noopener noreferrer" target="_blank" variant="body1" {...props} />
+        ),
         li: (props: unknown) => <Typography {...props} component="li" />,
         h2: (props: unknown) => <Typography component="h2" {...props} gutterBottom variant="h2" />,
+        InternalLink: ({ href, children, ref: _ref, ...props }: LinkProps) => (
+          <NextLink passHref href={href ?? '/'} {...props}>
+            <Link {...props}>{children}</Link>
+          </NextLink>
+        ),
       }}
     >
       {children}

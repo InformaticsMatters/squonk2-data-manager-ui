08a6f0da740312d5fd47a9f045392480b45e086f
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2022-05-28 22:45:42 +0100

    feat(nextjs): Add nicer transitions between routes

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,17 +1,23 @@
 import type { FC } from 'react';
 
-import { Box } from '@mui/material';
+import { Box, LinearProgress, Slide } from '@mui/material';
 
+import { useIsTransitioning } from '../hooks/useIsTransitioning';
 import { Footer } from './Footer';
 import Header from './Header';
 
 const Layout: FC = ({ children }) => {
+  const isTransitioning = useIsTransitioning(true);
+
   return (
     <Box display="flex" flexDirection="column" minHeight="100vh">
       <Header />
-      <Box component="main" paddingY={2}>
-        {children}
-      </Box>
+      {isTransitioning && <LinearProgress />}
+      <Slide appear direction="right" in={!isTransitioning}>
+        <Box component="main" paddingY={2}>
+          {children}
+        </Box>
+      </Slide>
       <Footer />
     </Box>
   );
---
+++ hooks/useIsTransitioning.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+import { useRouter } from 'next/router';
+
+export const useIsTransitioning = (defaultValue: boolean) => {
+  const [isTransitioning, setIsTransitioning] = useState(defaultValue);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const animationStart = () => setIsTransitioning(true);
+    const animationEnd = () => setIsTransitioning(false);
+
+    router.events.on('routeChangeStart', animationStart);
+    router.events.on('routeChangeComplete', animationEnd);
+    router.events.on('routeChangeError', animationEnd);
+
+    return () => {
+      router.events.off('routeChangeStart', animationStart);
+      router.events.off('routeChangeComplete', animationEnd);
+      router.events.off('routeChangeError', animationEnd);
+    };
+  }, [router]);
+
+  return isTransitioning;
+};

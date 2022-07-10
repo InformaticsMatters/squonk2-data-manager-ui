c3a662252c7e9f3b1d9330bef696bac05fb46222
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2022-07-10 13:03:53 +0100

    test(project-bootstrap): clean up existing default unit before testing

--- tests/project-bootstrap.spec.ts
+++ tests/project-bootstrap.spec.ts
@@ -1,11 +1,33 @@
 import { expect, test } from "@playwright/test";
 
-test("test", async ({ page, baseURL }) => {
-  const timestamp = String(Date.now());
-  const projectName = `playwright-${timestamp}`;
+test("Project bootstrap works", async ({ page, baseURL }) => {
+  // Display any logs to terminal (for debug)
+  page.on("console", async (msg) => {
+    console.log("Browser Log: ", msg);
+  });
 
   // Go to http://localhost:3000/
+  // This needs to come before the unit fetch request below so there isn't a cors issue
   await page.goto(baseURL ?? "/");
+
+  // Ensure default unit doesn't exist
+  await page.evaluate(
+    async ({ baseURL }) => {
+      const res = await fetch(baseURL + "/api/as-api/unit", { method: "DELETE" });
+      const response = await res.json();
+      if (!res.ok && response.error !== "The Unit does not exist") {
+        throw Error("A possibly existing unit could not be cleaned up before running the test");
+      }
+    },
+    { baseURL },
+  );
+
+  //
+  // The Test
+  //
+
+  const timestamp = String(Date.now());
+  const projectName = `playwright-${timestamp}`;
 
   // Click input[name="projectName"]
   await page.locator(`input[name="projectName"]`).click();
@@ -26,7 +48,7 @@ test("test", async ({ page, baseURL }) => {
   await page.locator(`button:has-text("Create")`).click();
 
   const regexp = new RegExp(
-    baseURL + "/\\?project=project-[\\w\\d]+-[\\w\\d]+-[\\w\\d]+-[\\w\\d]+-[\\w\\d]+",
+    baseURL + "/?\\?project=project-[\\w\\d]+-[\\w\\d]+-[\\w\\d]+-[\\w\\d]+-[\\w\\d]+",
   );
   await expect(page).toHaveURL(regexp);
 

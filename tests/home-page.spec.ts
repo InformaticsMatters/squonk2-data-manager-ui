eb4afacb6860d51bb2f2665ceba9e3e72c8090a1
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2022-07-25 21:07:24 +0100

    test(home-page): fix home-page tests

--- tests/home-page.spec.ts
+++ tests/home-page.spec.ts
@@ -1,5 +1,8 @@
 import { expect, test } from "@playwright/test";
 
+const squonkLogo =
+  "img[alt=\"Squonk \\(animal\\) logo with title text \\'Squonk\\' and subtitle \\'Data Manager\\'\"]";
+
 test.describe("home page", () => {
   test("loads", async ({ baseURL, page }) => {
     await page.goto(baseURL + "/");
@@ -13,54 +16,40 @@ test.describe("home page", () => {
 
     // Click text=Documentation
     await page.locator("text=Documentation").click();
-
     // Click text=Concepts
     await page.locator("text=Concepts").click();
     await expect(page).toHaveURL(baseURL + "/docs/concepts");
-
     // Click img[alt="Squonk \(animal\) logo with title text \'Squonk\' and subtitle \'Data Manager\'"]
-    await page
-      .locator(
-        "img[alt=\"Squonk \\(animal\\) logo with title text \\'Squonk\\' and subtitle \\'Data Manager\\'\"]",
-      )
-      .click();
+    await page.locator(squonkLogo).click();
     await expect(page).toHaveURL(baseURL);
-
     // Click text=Documentation
     await page.locator("text=Documentation").click();
-
     // Click text=Guided Tour
     await page.locator("text=Guided Tour").click();
     await expect(page).toHaveURL(baseURL + "/docs/guided-tour");
-
     // Click img[alt="Squonk \(animal\) logo with title text \'Squonk\' and subtitle \'Data Manager\'"]
-    await page
-      .locator(
-        "img[alt=\"Squonk \\(animal\\) logo with title text \\'Squonk\\' and subtitle \\'Data Manager\\'\"]",
-      )
-      .click();
+    await page.locator(squonkLogo).click();
     await expect(page).toHaveURL(baseURL);
-
     // Click text=Documentation
     await page.locator("text=Documentation").click();
-
-    // Click a:has-text("How to")
-    await page.locator(`a:has-text("How to")`).click();
+    // Click text=How To Guides
+    await page.locator("text=How To Guides").click();
     await expect(page).toHaveURL(baseURL + "/docs/how-to");
-
     // Click img[alt="Squonk \(animal\) logo with title text \'Squonk\' and subtitle \'Data Manager\'"]
-    await page
-      .locator(
-        "img[alt=\"Squonk \\(animal\\) logo with title text \\'Squonk\\' and subtitle \\'Data Manager\\'\"]",
-      )
-      .click();
+    await page.locator(squonkLogo).click();
     await expect(page).toHaveURL(baseURL);
-
     // Click text=Documentation
     await page.locator("text=Documentation").click();
-
     // Click text=Deployed jobs
     await page.locator("text=Deployed jobs").click();
     await expect(page).toHaveURL(baseURL + "/docs/jobs");
+    // Click img[alt="Squonk \(animal\) logo with title text \'Squonk\' and subtitle \'Data Manager\'"]
+    await page.locator(squonkLogo).click();
+    await expect(page).toHaveURL(baseURL);
+    // Click text=Documentation
+    await page.locator("text=Documentation").click();
+    // Click text=Developer docs
+    await page.locator("text=Developer docs").click();
+    await expect(page).toHaveURL(baseURL + "/docs/developer");
   });
 });

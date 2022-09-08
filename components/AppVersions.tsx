825a65062b4931fa78e2717ba2512ee7db996d25
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2022-09-08 17:42:37 +0100

    fix(footer): don't require login to see API versions

--- components/AppVersions.tsx
+++ components/AppVersions.tsx
@@ -1,24 +1,21 @@
 import { useQuery } from "react-query";
 
-import { getVersion as getASVersion } from "@squonk/account-server-client/state";
-import { getVersion as getDMVersion } from "@squonk/data-manager-client/accounting";
-
 import { ListItem, ListItemText, Typography } from "@mui/material";
+import axios from "axios";
 
-import { useIsAuthorized } from "../hooks/useIsAuthorized";
+import { AS_API_URL, DM_API_URL } from "../constants/proxies";
 import { HorizontalList } from "./HorizontalList";
 
 export const getGetDMVersionQueryKey = () => ["data-manager", "/version"];
 export const getGetASVersionQueryKey = () => ["account-server", "/version"];
 
 export const AppVersions = () => {
-  const isAuthorized = useIsAuthorized();
-  const { data: dmData } = useQuery(getGetDMVersionQueryKey(), getDMVersion, {
-    enabled: !!isAuthorized,
-  });
-  const { data: asData } = useQuery(getGetASVersionQueryKey(), getASVersion, {
-    enabled: !!isAuthorized,
-  });
+  const { data: dmData } = useQuery(getGetDMVersionQueryKey(), () =>
+    axios.get(DM_API_URL + "/version").then(({ data }) => data),
+  );
+  const { data: asData } = useQuery(getGetASVersionQueryKey(), () =>
+    axios.get(AS_API_URL + "/version").then(({ data }) => data),
+  );
 
   return (
     <>

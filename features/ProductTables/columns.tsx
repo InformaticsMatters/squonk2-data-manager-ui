23900455a2a6bcb57b6375183b31de418d79aafe
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2023-02-22 17:40:22 +0000

    fix(products): show correct header text with org and created columns

--- features/ProductTables/columns.tsx
+++ features/ProductTables/columns.tsx
@@ -10,10 +10,13 @@ export const getSharedColumns = <T extends ProductsGetResponseProductsItem>() =>
   return [
     columnHelper.accessor((row) => row.product.name, { header: "Name", id: "name" }),
     columnHelper.accessor((row) => toLocalTimeString(row.product.created, true, true), {
+      header: "Created",
+      id: "created",
+    }),
+    columnHelper.accessor((row) => row.organisation.name, {
       header: "Organisation",
       id: "organisation",
     }),
-    columnHelper.accessor((row) => row.organisation.name, { header: "Created", id: "created" }),
     columnHelper.accessor((row) => row.unit.name, { header: "Unit", id: "unit" }),
   ];
 };

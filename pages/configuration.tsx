5afc85ea0611b2a8de07cd0a2cb8b983c4eef57e
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2023-02-27 22:02:25 +0000

    fix(config-page): display current env values instead of statically built values

--- pages/configuration.tsx
+++ pages/configuration.tsx
@@ -1,14 +1,15 @@
 import { Container } from "@mui/material";
+import type { GetServerSideProps } from "next";
 
 // Format a value so undefined and empty string are visible
-const ReprLi = ({ title, children }: { children: string | undefined; title: string }) => {
+const ReprLi = ({ title, children }: { children: string | undefined | null; title: string }) => {
   if (children === "") {
     return (
       <li>
         {title}: <em>{"<empty string>"}</em>
       </li>
     );
-  } else if (children === undefined) {
+  } else if (children === undefined || children === null) {
     return (
       <li>
         {title}: <em>undefined</em>
@@ -22,21 +23,35 @@ const ReprLi = ({ title, children }: { children: string | undefined; title: stri
   );
 };
 
-export const Configuration = () => (
+export interface ConfigurationProps {
+  dmAPI: string | null;
+  asAPI: string | null;
+}
+
+export const getServerSideProps: GetServerSideProps<ConfigurationProps> = async () => {
+  // These may change between build and deployment. NextJS statically builds these so just using
+  // these "public" variables won't show the correct value.
+  // When undefined a null is passed as undefined isn't valid json.
+  const dmAPI = process.env.NEXT_PUBLIC_DATA_MANAGER_API_SERVER ?? null;
+  const asAPI = process.env.NEXT_PUBLIC_ACCOUNT_SERVER_API_SERVER ?? null;
+  return {
+    props: { dmAPI, asAPI },
+  };
+};
+
+export const Configuration = ({ dmAPI, asAPI }: ConfigurationProps) => (
   <Container>
     <h1>Configuration</h1>
     <p>
       Values will be displayed in <em>italics</em> if they are a special value (empty string or
-      undefined)
+      undefined).
     </p>
     <h2>Setup</h2>
     <ul>
       <ReprLi title="App Version">{process.env.NEXT_PUBLIC_APP_VERSION}</ReprLi>
       <ReprLi title="Base Path">{process.env.NEXT_PUBLIC_BASE_PATH}</ReprLi>
-      <ReprLi title="Profile">{process.env.NEXT_PUBLIC_AUTH0_PROFILE}</ReprLi>
-      <ReprLi title="Profile">{process.env.NEXT_PUBLIC_AUTH0_PROFILE}</ReprLi>
-      <ReprLi title="DM API Server">{process.env.NEXT_PUBLIC_DATA_MANAGER_API_SERVER}</ReprLi>
-      <ReprLi title="AS API Server">{process.env.NEXT_PUBLIC_ACCOUNT_SERVER_API_SERVER}</ReprLi>
+      <ReprLi title="DM API Server">{dmAPI}</ReprLi>
+      <ReprLi title="AS API Server">{asAPI}</ReprLi>
     </ul>
     <h2>Roles</h2>
     <ul>

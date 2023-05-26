a2837a3995781f73d3b1285a9d10db0bb42453ba
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2023-05-26 23:31:20 +0100

    test(playwright): ensure tests run on a base path and custom port

--- tests/navigation.browser.ts
+++ tests/navigation.browser.ts
@@ -2,19 +2,13 @@ import { test } from "@playwright/test";
 
 import { login } from "./login";
 
-const { BASE_URL, BASE_PATH = "" } = process.env;
-
-test.describe("home page", () => {
-  test("loads", async ({ baseURL, page }) => {
-    await page.goto(baseURL + "/");
-    await page.getByRole("heading", { name: "Documentation" }).waitFor();
-  });
+test("loads", async ({ baseURL, page }) => {
+  await page.goto(baseURL as string);
+  await page.getByRole("heading", { name: "Documentation" }).waitFor();
 });
 
-test("do login via results page", async ({ page }) => {
-  const baseURL = "" + BASE_URL + BASE_PATH;
-
-  await page.goto(baseURL);
+test("do login via results page", async ({ page, baseURL }) => {
+  await page.goto(baseURL as string);
   await page
     .getByRole("navigation", { name: "Main" })
     .getByRole("link", { name: "Results" })
@@ -24,14 +18,12 @@ test("do login via results page", async ({ page }) => {
   await login(page);
 
   // We should return to the page we clicked on
-  await page.waitForURL(BASE_URL + "/results");
+  await page.waitForURL("**/results");
   await page.locator("label").filter({ hasText: "Filter Results" }).waitFor();
 });
 
-test("do login via datasets page", async ({ page }) => {
-  const baseURL = "" + BASE_URL + BASE_PATH;
-
-  await page.goto(baseURL);
+test("do login via datasets page", async ({ page, baseURL }) => {
+  await page.goto(baseURL as string);
   await page
     .getByRole("navigation", { name: "Main" })
     .getByRole("link", { name: "Datasets" })
@@ -41,14 +33,12 @@ test("do login via datasets page", async ({ page }) => {
   await login(page);
 
   // We should return to the page we clicked on
-  await page.waitForURL(BASE_URL + "/datasets");
+  await page.waitForURL("**/datasets");
   await page.getByRole("heading", { name: "Datasets" }).waitFor();
 });
 
-test("do login via executions page", async ({ page }) => {
-  const baseURL = "" + BASE_URL + BASE_PATH;
-
-  await page.goto(baseURL);
+test("do login via executions page", async ({ page, baseURL }) => {
+  await page.goto(baseURL as string);
   await page
     .getByRole("navigation", { name: "Main" })
     .getByRole("link", { name: "Executions" })
@@ -58,14 +48,12 @@ test("do login via executions page", async ({ page }) => {
   await login(page);
 
   // We should return to the page we clicked on
-  await page.waitForURL(BASE_URL + "/executions");
+  await page.waitForURL("**/executions");
   await page.locator("label").filter({ hasText: "Filter Executions" }).waitFor();
 });
 
-test("do login via project page", async ({ page }) => {
-  const baseURL = "" + BASE_URL + BASE_PATH;
-
-  await page.goto(baseURL);
+test("do login via project page", async ({ page, baseURL }) => {
+  await page.goto(baseURL as string);
   await page
     .getByRole("navigation", { name: "Main" })
     .getByRole("link", { name: "Project" })
@@ -75,6 +63,6 @@ test("do login via project page", async ({ page }) => {
   await login(page);
 
   // We should return to the page we clicked on
-  await page.waitForURL(BASE_URL + "/project");
+  await page.waitForURL("**/project");
   await page.getByRole("heading", { name: "Select a project to view" }).waitFor();
 });

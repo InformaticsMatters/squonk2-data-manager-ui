787b5e86c0d48d56daee67fce85066f14fe5c851
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2023-07-14 22:19:00 +0100

    refactor(auth): get roles from access token instead of needing a keycloak role mapping

--- pages/api/auth/[auth0].ts
+++ pages/api/auth/[auth0].ts
@@ -1,5 +1,6 @@
-import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
+import { handleAuth, handleLogin, handleProfile } from "@auth0/nextjs-auth0";
 import { captureException } from "@sentry/nextjs";
+import jwtDecode from "jwt-decode";
 
 export default handleAuth({
   login: handleLogin({
@@ -12,6 +13,27 @@ export default handleAuth({
     console.error(error);
     res.status(error.status || 500).end();
     captureException(error);
+  },
+  profile: async (req, res) => {
+    await handleProfile(req, res, {
+      refetch: true,
+      afterRefetch: (_req, _res, newSession) => {
+        // add the roles from the access token to the user object
+        const accessToken = newSession.accessToken;
+        if (typeof accessToken !== "string") {
+          return newSession;
+        }
+
+        const decodedAccessToken = jwtDecode<Record<string, any>>(accessToken);
+        return {
+          ...newSession,
+          user: {
+            ...newSession.user,
+            realm_access: decodedAccessToken.realm_access,
+          },
+        };
+      },
+    });
   },
 });
 

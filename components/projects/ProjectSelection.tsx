fa62ccaa5d740be5766d56d07e3510ff0b746350
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2024-04-23 21:51:51 +0100

    fix(project-selection): fix alignment of unit and org inputs

--- components/projects/ProjectSelection.tsx
+++ components/projects/ProjectSelection.tsx
@@ -7,13 +7,13 @@ import { SelectUnit } from "../userContext/SelectUnit";
 export const ProjectSelection = () => {
   return (
     <Grid container spacing={1}>
-      <Grid container item alignItems="center" sm={6}>
+      <Grid container item alignItems="baseline" sm={6}>
         <SelectOrganisation />
       </Grid>
-      <Grid container item alignItems="center" sm={6}>
+      <Grid container item alignItems="baseline" sm={6}>
         <SelectUnit userFilter={["none"]} />
       </Grid>
-      <Grid container item alignItems="center" sm={12}>
+      <Grid container item alignItems="baseline" sm={12}>
         <SelectProject size="medium" />
       </Grid>
     </Grid>

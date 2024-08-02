133e4106b162dbbb1490f3a2f32f48e254550943
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2024-08-02 08:42:52 +0100

    feat: include missing units to organisation usage page

--- src/components/usage/OrganisationUserUsage.tsx
+++ src/components/usage/OrganisationUserUsage.tsx
@@ -7,9 +7,10 @@ dayjs.extend(utc);
 
 import { type UserDetail } from "@squonk/account-server-client";
 import { useGetOrganisation } from "@squonk/account-server-client/organisation";
-import { useGetUnits } from "@squonk/account-server-client/unit";
+import { useGetOrganisationUnits } from "@squonk/account-server-client/unit";
 import { useGetOrganisationUsers } from "@squonk/account-server-client/user";
 import { type InventoryUserDetail } from "@squonk/data-manager-client";
+import { useGetProjects } from "@squonk/data-manager-client/project";
 
 import { Alert, Container, Typography } from "@mui/material";
 import { createColumnHelper } from "@tanstack/react-table";
@@ -22,6 +23,7 @@ import { getSharedColumns, type UserActivity } from "./sharedColumns";
 type Unit = {
   id: string;
   name?: string;
+  number_of_projects: number;
 };
 
 type InventoryWithUnit = UserActivity & {
@@ -36,47 +38,99 @@ export interface OrganisationUserUsageProps {
   organisationId: string;
 }
 
-export const OrganisationUserUsage = ({ organisationId }: OrganisationUserUsageProps) => {
-  const { data: organisation, isLoading: isOrganisationLoading } =
-    useGetOrganisation(organisationId);
-  const { data: units } = useGetUnits({
-    query: { select: (data) => data.units.flatMap((org) => org.units) },
+const useOranisationUsersData = (organisationId: string) => {
+  const { data: organisation } = useGetOrganisation(organisationId);
+  const { data: units } = useGetOrganisationUnits(organisationId, {
+    query: { select: (data) => data.units },
   });
-  const { data, error: inventoryError } = useGetUserInventory<InventoryWithUnit[]>(
+  const { data: allProjects } = useGetProjects(undefined, {
+    query: { select: (data) => data.projects },
+  });
+  const { data, error } = useGetUserInventory<InventoryWithUnit[]>(
     { org_id: organisationId },
     {
       query: {
         retry: false,
         select: (data) => {
-          return data.users.map(({ projects, activity, first_seen, last_seen_date, username }) => ({
-            username,
-            activity,
-            first_seen,
-            last_seen_date,
-            units: Object.values(projects)
-              .flat()
-              .map(({ unit_id }) => ({
-                id: unit_id,
-                name: units?.find((unit) => unit.id === unit_id)?.name,
-              }))
-              .reduce<Unit[]>((uniqueUnits, unit) => {
-                // keep only unique units
-                const existingUnit = uniqueUnits.find((u) => u.id === unit.id);
-                if (!existingUnit) {
-                  uniqueUnits.push(unit);
-                }
-                return uniqueUnits;
-              }, []),
-          }));
+          return data.users
+            .map(({ projects, activity, first_seen, last_seen_date, username }) => {
+              // get all the unique units from the projects
+              // include the unit name from the units response
+              const inventoryUnits = Object.values(projects)
+                .flat()
+                .map(({ unit_id }) => ({
+                  id: unit_id,
+                  name: units?.find((unit) => unit.id === unit_id)?.name,
+                  number_of_projects: Object.values(projects)
+                    .flat()
+                    .reduce((count, project) => {
+                      return project.unit_id === unit_id ? count + 1 : count;
+                    }, 0),
+                }))
+                .reduce<Unit[]>((uniqueUnits, unit) => {
+                  // keep only unique units
+                  const existingUnit = uniqueUnits.find((u) => u.id === unit.id);
+                  if (!existingUnit) {
+                    uniqueUnits.push(unit);
+                  }
+                  return uniqueUnits;
+                }, []);
+
+              // include the units from the units response missing from the inventory that the user
+              // is a member
+              const newUnits: Unit[] =
+                units
+                  ?.filter((unit) => {
+                    const foundUnit = inventoryUnits.find((u) => u.id !== unit.id);
+                    if (foundUnit === undefined) {
+                      return false;
+                    } else if (
+                      (organisation?.users ?? []).map((user) => user.id).includes(username) ||
+                      organisation?.owner_id === username ||
+                      unit.owner_id === username ||
+                      unit.users.map((user) => user.id).includes(username)
+                    ) {
+                      return true;
+                    }
+                    return false;
+                  })
+                  .map((unit) => ({
+                    id: unit.id,
+                    name: unit.name,
+                    number_of_projects:
+                      allProjects?.filter((project) => project.unit_id === unit.id).length ?? 0,
+                  })) ?? [];
+
+              const allUnits = [...inventoryUnits, ...newUnits];
+
+              return {
+                username,
+                activity,
+                first_seen,
+                last_seen_date,
+
+                units: allUnits,
+              };
+            })
+            .filter((user) => user.units.length > 0);
         },
       },
     },
   );
+
+  return { data, error };
+};
+
+export const OrganisationUserUsage = ({ organisationId }: OrganisationUserUsageProps) => {
+  const { data: organisation, isLoading: isOrganisationLoading } =
+    useGetOrganisation(organisationId);
+
   const { data: organisationMembers } = useGetOrganisationUsers(organisationId, {
     query: {
       enabled: isOrganisationLoading || !organisation || organisation.caller_is_member,
     },
   });
+  const { data, error: inventoryError } = useOranisationUsersData(organisationId);
 
   const columns = [
     columnHelper.accessor("username", { header: "User" }),
@@ -94,7 +148,7 @@ export const OrganisationUserUsage = ({ organisationId }: OrganisationUserUsageP
                   component="a"
                   href={{ pathname: "/unit/[unitId]/inventory", query: { unitId: unit.id } }}
                 >
-                  {unit.name}
+                  {unit.name ?? unit.id} ({unit.number_of_projects})
                 </NextLink>
               </li>
             ))}

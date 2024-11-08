9a7b6c76cffd5d13bd388c26cb22093c1a11df10
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2024-11-08 22:10:32 +0000

    chore: use scss @use instead of @import

--- next.config.mjs
+++ next.config.mjs
@@ -37,10 +37,7 @@ let nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || undefined,
   transpilePackages: MONOREPO_MODE ? ["@squonk/mui-theme", "@squonk/sdf-parser"] : [],
   sassOptions: {
-    prependData: `$assetsURL: '${
-      // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-      process.env.ASSET_URL || "https://squonk.informaticsmatters.org"
-    }';`,
+    prependData: `$assetsURL: '${process.env.ASSET_URL}';`,
   },
   images: {
     domains: ["squonk.informaticsmatters.org"],

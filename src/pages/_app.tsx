300b8adf3765769dd8215be1971db71bbed72c93
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2024-11-30 10:39:54 +0000

    refactor: use mui nextjs integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,25 @@
-import { useEffect, useMemo } from "react";
+import { useMemo } from "react";
 
 import { setBaseUrl as setASBaseUrl } from "@squonk/account-server-client";
 import { setBaseUrl as setDMBaseUrl } from "@squonk/data-manager-client";
 
 import { UserProvider } from "@auth0/nextjs-auth0/client";
-import { type EmotionCache } from "@emotion/cache";
-import { CacheProvider } from "@emotion/react";
+import {
+  AppCacheProvider,
+  type EmotionCacheProviderProps,
+} from "@mui/material-nextjs/v15-pagesRouter";
 import { HydrationBoundary, QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { enableMapSet } from "immer";
 import { type AppProps } from "next/app";
-import Head from "next/head";
 import { SnackbarProvider } from "notistack";
 
 import { ThemeProviders } from "../components/app/ThemeProviders";
 import { TopLevelHooks } from "../components/app/TopLevelHooks";
 import { AS_API_URL, DM_API_URL } from "../constants/proxies";
 import { MDXComponentProvider } from "../context/MDXComponentProvider";
-import createEmotionCache from "../utils/next/createEmotionCache";
 
 import "../styles/globalStyles.scss";
-
-const clientSideEmotionCache = createEmotionCache();
 
 setDMBaseUrl(DM_API_URL);
 setASBaseUrl(AS_API_URL);
@@ -31,24 +29,14 @@ enableMapSet();
 // Adjust template for MUI given at
 // https://github.com/mui/material-ui/blob/master/examples/nextjs-with-typescript/pages/_app.tsx
 
-type CustomAppProps = AppProps & {
-  emotionCache?: EmotionCache;
-  pageProps: { dehydratedState?: unknown };
-};
+type CustomAppProps = AppProps &
+  EmotionCacheProviderProps & {
+    pageProps: { dehydratedState?: unknown };
+  };
 
-const App = ({ Component, pageProps, emotionCache = clientSideEmotionCache }: CustomAppProps) => {
+const App = ({ Component, pageProps }: CustomAppProps) => {
   // React-Query
   const queryClient = useMemo(() => new QueryClient(), []);
-
-  // Material UI for SSR
-  useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector("#jss-server-side");
-    if (jssStyles) {
-      // eslint-disable-next-line unicorn/prefer-dom-node-remove
-      jssStyles.parentElement?.removeChild(jssStyles);
-    }
-  }, []);
 
   // Vercel specific code is only imported if needed
   // if (process.env.NEXT_PUBLIC_VERCEL_URL) {
@@ -56,10 +44,7 @@ const App = ({ Component, pageProps, emotionCache = clientSideEmotionCache }: Cu
   // }
 
   return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <meta content="minimum-scale=1, initial-scale=1, width=device-width" name="viewport" />
-      </Head>
+    <AppCacheProvider {...pageProps}>
       <ThemeProviders>
         <UserProvider
           loginUrl={`${process.env.NEXT_PUBLIC_BASE_PATH ?? ""}/api/auth/login`}
@@ -79,7 +64,7 @@ const App = ({ Component, pageProps, emotionCache = clientSideEmotionCache }: Cu
           </QueryClientProvider>
         </UserProvider>
       </ThemeProviders>
-    </CacheProvider>
+    </AppCacheProvider>
   );
 };
 

573cede208249a6282f7bf85a01b241e36ca9f0a
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2025-12-30 20:52:16 +0000

    test: refactor test to use dev server rather than production build

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,8 @@ import path from "node:path";
 require("dotenv").config({ path: path.resolve(process.cwd(), ".env.test.local") });
 
 const baseURL = new URL(process.env.BASE_URL as string);
-baseURL.pathname = process.env.BASE_PATH ?? "/";
+const BASE_PATH = process.env.BASE_PATH ?? "/";
+baseURL.pathname = BASE_PATH;
 const PORT = process.env.TEST_PORT ?? "3000";
 baseURL.port = PORT;
 
@@ -13,10 +14,7 @@ export default defineConfig({
     { name: "setup", testMatch: "**/*.setup.ts" },
     {
       name: "browser",
-      use: {
-        baseURL: baseURL.href,
-        trace: "on",
-      },
+      use: { baseURL: baseURL.href, trace: "on" },
       retries: 0,
       timeout: 60_000,
       testMatch: "**/*.browser.ts",
@@ -24,11 +22,7 @@ export default defineConfig({
     {
       name: "browser-authenticated",
       dependencies: ["setup"],
-      use: {
-        storageState: "storageState.json",
-        baseURL: baseURL.href,
-        trace: "on",
-      },
+      use: { storageState: "storageState.json", baseURL: baseURL.href, trace: "on" },
       retries: 3,
       timeout: 60_000,
       testMatch: "**/*.browser-authenticated.ts",
@@ -40,17 +34,13 @@ export default defineConfig({
     trace: "on", // record traces on first retry of each test
   },
   webServer: {
-    // needs to use the package manager here to avoid an error when not running playwright from the
-    // terminal
-    command: `pnpm start -p ${PORT}`,
+    command: `pnpm dev -p ${PORT}`,
     url: baseURL.href,
     timeout: 200 * 1000,
     reuseExistingServer: true,
     stdout: "pipe",
     stderr: "pipe",
-    env: {
-      NODE_ENV: "test",
-    },
+    env: { NODE_ENV: "test" },
   },
   testDir: "tests",
 });

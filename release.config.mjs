989d3436999674a9159b6ad23ff31759dd806bda
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2026-01-13 16:16:12 +0000

    ci: disable releasedLabels and successComment

--- release.config.mjs
+++ release.config.mjs
@@ -16,7 +16,7 @@ const config = {
         message: "chore: release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
-    "@semantic-release/github",
+    ["@semantic-release/github", { releasedLabels: false, successComment: false }],
   ],
 };
 
